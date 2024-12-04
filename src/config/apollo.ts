1cebbbad31015b831692f0e50d68fd1dde4f23bc
Alexey Sarakuz <homahomchikov@gmail.com>
2024-12-04 02:49:12 +0200

    fix(env): hardcoded env variable

--- src/config/apollo.ts
+++ src/config/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
-  uri: `${import.meta.env.VITE_API_URL}/graphql`,
+  uri: `${import.meta.env.VITE_API_URL || "http://ec2-18-221-79-239.us-east-2.compute.amazonaws.com:9000"}/graphql`,
   cache: new InMemoryCache(),
 });
